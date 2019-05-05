b1a70aefea5ea7cef624c3341c1702fc03a01771
Amir Mohsen Abdolrazaghi <amirmohsen.a@gmail.com>
2019-05-06 00:55:17 +0100

    hacky fix for styling issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 import 'typeface-roboto';
@@ -6,23 +6,55 @@ import Masthead from '../components/Masthead';
 import SellingPoints from '../components/SellingPoints';
 import ConfigurableCodeBlock from '../components/ConfigurableCodeBlock';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Masthead />
-    <SellingPoints />
-    <ConfigurableCodeBlock>
-      {`
-        import { Money } from 'wealth';
-        import { add, remove, parse } from 'wealth/fn';
+class IndexPage extends Component {
+  state = {
+    load: false
+  };
+  componentDidMount() {
+    this.setState({
+      load: true
+    });
+  }
 
-        parse('$15.85');
-        const moneyA = new Money('15.00', 'USD');
-        const moneyB = new Money('30.00', 'USD');
-        const total = add(moneyA, moneyB);
-      `}
-    </ConfigurableCodeBlock>
-  </Layout>
-);
+  render() {
+    return this.state.load && (
+      <Layout>
+        <SEO title="Home" />
+        <Masthead />
+        <SellingPoints />
+        <ConfigurableCodeBlock>
+          {`
+            import { Money } from 'wealth';
+            import { add, remove, parse } from 'wealth/fn';
+
+            parse('$15.85');
+            const moneyA = new Money('15.00', 'USD');
+            const moneyB = new Money('30.00', 'USD');
+            const total = add(moneyA, moneyB);
+          `}
+        </ConfigurableCodeBlock>
+      </Layout>
+    );
+  }
+}
+
+// const IndexPage = () => (
+//   <Layout>
+//     <SEO title="Home" />
+//     <Masthead />
+//     <SellingPoints />
+//     <ConfigurableCodeBlock>
+//       {`
+//         import { Money } from 'wealth';
+//         import { add, remove, parse } from 'wealth/fn';
+
+//         parse('$15.85');
+//         const moneyA = new Money('15.00', 'USD');
+//         const moneyB = new Money('30.00', 'USD');
+//         const total = add(moneyA, moneyB);
+//       `}
+//     </ConfigurableCodeBlock>
+//   </Layout>
+// );
 
 export default IndexPage;
