8b39283ad97c6f23498cfda0a69a296706a1bf88
Amir Mohsen Abdolrazaghi <amirmohsen.a@gmail.com>
2019-09-23 00:45:59 +0100

    replace popover with drawer for mobile nav

--- src/components/Header/GitHubLink/index.js
+++ src/components/Header/GitHubLink/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import IconButton from '@material-ui/core/IconButton';
-import styled from 'styled-components';
-import ghIcon from './gh-icon.png';
+import React from 'react'
+import IconButton from '@material-ui/core/IconButton'
+import styled from 'styled-components'
+import ghIcon from './gh-icon.png'
 
 const GitHubIcon = styled.img.attrs({
-    src: ghIcon,
-    alt: 'Github Icon'
+  src: ghIcon,
+  alt: 'Github Icon',
 })`
-    display: block;
-    width: 30px;
-    height: 30px;
-`;
+  display: block;
+  width: 30px;
+  height: 30px;
+`
 
 const GitHubLink = () => (
-    <IconButton
-        aria-label="Wealth GitHub Link"
-        component="a"
-        target="_blank"
-        href="https://github.com/amirmohsen/wealth"
-    >
-        <GitHubIcon />
-    </IconButton>
-);
+  <IconButton
+    aria-label="Wealth GitHub Link"
+    component="a"
+    target="_blank"
+    href="https://github.com/amirmohsen/wealth"
+  >
+    <GitHubIcon />
+  </IconButton>
+)
 
-export default GitHubLink;
+export default GitHubLink
--- src/components/Header/Navigation/MobileNavigation.js
+++ src/components/Header/Navigation/MobileNavigation.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import IconButton from '@material-ui/core/IconButton';
-import MenuInnerIcon from '@material-ui/icons/Menu';
-import { Link } from 'gatsby';
-import styled from 'styled-components';
+import React, { Component } from 'react'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow'
+import MenuInnerIcon from '@material-ui/icons/Menu'
+import HelpIcon from '@material-ui/icons/Help'
+import IconButton from '@material-ui/core/IconButton'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 const MenuIcon = styled(MenuInnerIcon)`
   color: white;
-`;
+`
 
 const MenuLink = styled(Link)`
   &,
@@ -20,72 +22,75 @@ const MenuLink = styled(Link)`
     color: black;
     text-decoration: none;
   }
-`;
+`
+
+const Wrapper = styled.div`
+  width: 300px;
+`
 
 export default class MobileNavigation extends Component {
   state = {
     open: false,
-  };
+  }
 
-  handleToggle = () => {
-    this.setState(state => ({ open: !state.open }));
-  };
-
-  handleClose = event => {
-    if (this.anchorEl.contains(event.target)) {
-      return;
+  handleDrawer = open => event => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return
     }
-
-    this.setState({ open: false });
-  };
+    this.setState({ open })
+  }
 
   render() {
-    const { open } = this.state;
+    const { open } = this.state
 
     return (
       <>
-        <IconButton
-          buttonRef={node => {
-            this.anchorEl = node;
-          }}
-          aria-owns={open ? 'menu-list-grow' : undefined}
-          aria-haspopup="true"
-          onClick={this.handleToggle}
-        >
+        <IconButton onClick={this.handleDrawer(true)}>
           <MenuIcon fontSize="large" />
         </IconButton>
-        <Popper open={open} anchorEl={this.anchorEl} transition disablePortal>
-          {({ TransitionProps, placement }) => (
-            <Grow
-              {...TransitionProps}
-              id="menu-list-grow"
-              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-            >
-              <Paper>
-                <ClickAwayListener onClickAway={this.handleClose}>
-                  <MenuList>
-                    <MenuItem onClick={this.handleClose}>
-                      <MenuLink to="/getting-started">
-                        Getting Started
-                      </MenuLink>
-                    </MenuItem>
-                    <MenuItem onClick={this.handleClose}>
-                      <MenuLink to="/guides">
-                        Guides
-                      </MenuLink>
-                    </MenuItem>
-                    {/* <MenuItem onClick={this.handleClose}>
-                      <MenuLink to="/reference">
-                        API Reference
-                      </MenuLink>
-                    </MenuItem> */}
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
+        <SwipeableDrawer
+          anchor="right"
+          open={open}
+          onClose={this.handleDrawer(false)}
+          onOpen={this.handleDrawer(true)}
+        >
+          <Wrapper
+            role="presentation"
+            onClick={this.handleDrawer(false)}
+            onKeyDown={this.handleDrawer(false)}
+          >
+            <List>
+              <ListItem
+                to="/getting-started"
+                component={MenuLink}
+                key="getting-started"
+              >
+                <ListItemIcon>
+                  <PlayArrowIcon />
+                </ListItemIcon>
+                <ListItemText primary="Getting Started" />
+              </ListItem>
+              <Divider />
+              <ListItem to="/guides" component={MenuLink} key="guides">
+                <ListItemIcon>
+                  <HelpIcon />
+                </ListItemIcon>
+                <ListItemText primary="Guides" />
+              </ListItem>
+            </List>
+            {/* <MenuItem onClick={this.handleClose}>
+                <MenuLink to="/reference">
+                  API Reference
+                </MenuLink>
+              </MenuItem> */}
+            {/* </MenuList> */}
+          </Wrapper>
+        </SwipeableDrawer>
       </>
-    );
+    )
   }
 }
